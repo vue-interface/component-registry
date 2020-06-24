9f50bb63ca8cbecf39da6d02d40f2920c594f90a
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro-2.local>
2020-06-24 14:44:25 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+
+export default class ComponentRegistry {
+
+    constructor(components = {}) {
+        this.components = components;
+    }
+
+    validate(value) {
+        if(typeof value === 'object') {
+            return value;
+        }
+
+        throw new Error(
+            `Invalid data type \`${typeof value}\`. Should be type \`object\`.`
+        );
+    }
+
+    get(name) {
+        if(this.components[name]) {
+            return this.components[name];
+        }
+
+        throw new Error(`"${name}" has not been registered yet!`);
+    }
+
+    register(name, value) {
+        if(typeof name === 'object') {
+            Object.entries(name).forEach(([name, module]) => {
+                this.register(name, module);
+            });
+
+            return this;
+        }
+
+        this.components[name] = this.validate(value);
+
+        return this;
+    }
+
+    remove(name) {
+        delete this.components[name];
+
+        return this;
+    }
+
+    reset() {
+        this.components = {};
+    }
+
+}
