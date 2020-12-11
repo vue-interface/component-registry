a6b9a3c9ef9178000684e331dbe51997eed800f8
Hostinger <hello@mntarts.org>
2020-12-11 00:53:52 -0700

    pre-release commit

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     presets: [
         ['@vue/app', {
+            corejs: 3,
+            useBuiltIns: "usage", // or "entry"
             targets: {
-                node: "current",
-                browsers: "> 0.25%, not dead"
+                ie: 10,
+                ios: 9,
+                browsers: "> .25%, not dead",
             }
         }]
     ],
