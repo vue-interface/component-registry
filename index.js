8a4413b950d0d40e964b3fd2c18e13efb8d71928
Justin Kimbrell <jkimbrell@actengage.com>
2022-11-18 12:50:18 -0700

    Fixed TS issues

--- index.js
+++
@@ -1,5 +0,0 @@
-import ComponentRegistry from './src/ComponentRegistry';
-function factory(components = {}) {
-    return new ComponentRegistry(components);
-}
-export { factory, ComponentRegistry };
